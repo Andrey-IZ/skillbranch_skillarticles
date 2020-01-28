4ef7a447df2ac5ff8b8f836b17303e5aeb5c0851
andrew <zaporozhtsev@2211.grad.rnd>
2020-01-28 18:24:27 +0300

    UI: rootactivity + search menu

--- app/src/main/java/ru/skillbranch/skillarticles/ui/RootActivity.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/RootActivity.kt
@@ -1,51 +1,146 @@
 package ru.skillbranch.skillarticles.ui
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.View
+import android.view.Menu
 import android.widget.ImageView
-import android.widget.Toolbar
+import android.widget.SearchView
 import androidx.appcompat.app.AppCompatDelegate
-import androidx.core.view.children
+import androidx.appcompat.widget.Toolbar
+import androidx.lifecycle.ViewModelProvider
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_root.*
 import kotlinx.android.synthetic.main.layout_bootombar.*
 import kotlinx.android.synthetic.main.layout_submenu.*
 import ru.skillbranch.skillarticles.R
 import ru.skillbranch.skillarticles.extensions.dpToIntPx
+import ru.skillbranch.skillarticles.viewmodels.ArticleState
+import ru.skillbranch.skillarticles.viewmodels.ArticleViewModel
+import ru.skillbranch.skillarticles.viewmodels.Notify
+import ru.skillbranch.skillarticles.viewmodels.ViewModelFactory
+import java.util.zip.Inflater
 
 class RootActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: ArticleViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_root)
 
         setupToolbar()
+        setupBottomBar()
+        setupSubmenu()
 
-        btn_like.setOnClickListener {
-            Snackbar.make(coordinator_container, "test", Snackbar.LENGTH_LONG)
-                .setAnchorView(bottombar)
-                .show()
+        val vmFactory = ViewModelFactory("0")
+        viewModel = ViewModelProvider(this, vmFactory).get(ArticleViewModel::class.java)
+        viewModel.observeState(this) {
+            renderUI(it)
         }
+        viewModel.observeNotifications(this) {
+            renderNotifications(it)
+        }
+        this.btn_share.layoutParams
+    }
 
-        switch_mode.setOnClickListener {
-            delegate.localNightMode = if (switch_mode.isChecked) AppCompatDelegate.MODE_NIGHT_YES else AppCompatDelegate.MODE_NIGHT_NO
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_search, menu)
+        with(menu?.findItem(R.id.action_search)?.actionView as SearchView) {
+            queryHint = "search"
         }
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    private fun renderNotifications(notify: Notify) {
+        val snackbar = Snackbar.make(coordinator_container, notify.message, Snackbar.LENGTH_LONG)
+            .setAnchorView(bottombar)
+
+        when (notify) {
+            is Notify.TextMessage -> { /* nothing */ }
+
+            is Notify.ActionMessage -> {
+                snackbar.setActionTextColor(getColor(R.color.color_accent_dark))
+                snackbar.setAction(notify.actionLabel) {
+                    notify.actionHandler.invoke()
+                }
+            }
+
+            is Notify.ErrorMessage -> {
+                with(snackbar) {
+                    setBackgroundTint(getColor(R.color.design_default_color_error))
+                    setTextColor(getColor(android.R.color.white))
+                    setActionTextColor(getColor(android.R.color.white))
+                    setAction(notify.errLabel) {
+                        notify.errHandler?.invoke()
+                    }
+                }
+            }
+        }
+        snackbar.show()
+    }
+
+    private fun setupSubmenu() {
+        btn_text_down.setOnClickListener { viewModel.handleDownText() }
+        btn_text_up.setOnClickListener { viewModel.handleUpText() }
+        switch_mode.setOnClickListener { viewModel.handleNightMode() }
+    }
+
+    private fun setupBottomBar() {
+        btn_like.setOnClickListener { viewModel.handleLike() }
+        btn_bookmark.setOnClickListener { viewModel.handleBookmark() }
+        btn_settings.setOnClickListener { viewModel.handleToggleMenu() }
+        btn_share.setOnClickListener { viewModel.handleShare() }
+    }
+
+    private fun renderUI(data: ArticleState) {
+        Log.v(getString(R.string.TAG), "renderUI($data)")
+
+        // bind submenu state
+        btn_settings.isChecked = data.isShowMenu
+        if (data.isShowMenu) submenu.open() else submenu.close()
+
+        Log.v(getString(R.string.TAG), "data.isLike = ${data.isLike}")
+        // bind article person data
+        btn_like.isChecked = data.isLike
+        btn_bookmark.isChecked = data.isBookmark
+
+        // bind submenu views
+        switch_mode.isChecked = data.isDarkMode
+        delegate.localNightMode =
+            if (data.isDarkMode) AppCompatDelegate.MODE_NIGHT_YES else AppCompatDelegate.MODE_NIGHT_NO
+
+        if (data.isBigText) {
+            tv_text_content.textSize = 18f
+        } else {
+            tv_text_content.textSize = 14f
+        }
+        btn_text_up.isChecked = data.isBigText
+        btn_text_down.isChecked = !data.isBigText
+
+        // bind content
+        tv_text_content.text = if (data.isLoadingContent) "loading..." else data.content.first() as String
+
+        //bind toolbar
+        toolbar.title = data.title ?: "loading"
+        toolbar.subtitle = data.category ?: "loading"
+        if (data.category !=null) toolbar.logo = getDrawable(data.categoryIcon as Int)
     }
 
     private fun setupToolbar() {
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        val logo = if (toolbar.childCount > 2) toolbar.getChildAt(2) as? ImageView else null
+        val logo = if (toolbar.childCount > 2) toolbar.getChildAt(2) as ImageView else null
         logo?.scaleType = ImageView.ScaleType.CENTER_CROP
 
         val lp = logo?.layoutParams as? Toolbar.LayoutParams
-        lp?.apply {
-            width = dpToIntPx(40)
-            height = dpToIntPx(40)
-            marginEnd = dpToIntPx(16)
+        lp?.let {
+            it.width = dpToIntPx(40)
+            it.height = dpToIntPx(40)
+            it.marginEnd = dpToIntPx(16)
+            logo.layoutParams = it
         }
 
     }
