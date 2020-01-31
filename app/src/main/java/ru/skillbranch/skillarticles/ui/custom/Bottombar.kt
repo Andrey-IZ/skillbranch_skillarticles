83938da613014669efecc79dfc6c7a1c872433a7
andrew <zaporozhtsev@2211.grad.rnd>
2020-01-31 14:29:02 +0300

    UI: bottombar copied off master

--- app/src/main/java/ru/skillbranch/skillarticles/ui/custom/Bottombar.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/custom/Bottombar.kt
@@ -5,19 +5,25 @@ import android.util.AttributeSet
 import android.view.View
 import android.widget.FrameLayout
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.coordinatorlayout.widget.CoordinatorLayout
 import com.google.android.material.shape.MaterialShapeDrawable
 import ru.skillbranch.skillarticles.R
+import ru.skillbranch.skillarticles.ui.custom.behaviors.BottombarBehavior
 
 class Bottombar @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
-) : ConstraintLayout(context, attrs, defStyleAttr) {
+) : ConstraintLayout(context, attrs, defStyleAttr), CoordinatorLayout.AttachedBehavior {
 
     init {
       View.inflate(context, R.layout.layout_bootombar, this)
         val materialBg = MaterialShapeDrawable.createWithElevationOverlay(context, elevation)
 //        materialBg.elevation = elevation
         background = materialBg
+    }
+
+    override fun getBehavior(): CoordinatorLayout.Behavior<Bottombar> {
+        return BottombarBehavior()
     }
 }
